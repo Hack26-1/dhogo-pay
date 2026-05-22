9372295be629903302984b8dbc3d72ba007f422e
Hack26-1 <ngonisatakunda04@gmail.com>
2026-05-22 16:24:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,58 @@
-q
+async function payNow(){
+
+  const phone =
+    document.getElementById("phone").value;
+
+  const status =
+    document.getElementById("status");
+
+  /* Validation */
+
+  if(phone.trim() === ""){
+
+    status.innerText =
+      "Please enter EcoCash number";
+
+    return;
+  }
+
+  status.innerText =
+    "Sending payment request...";
+
+  try{
+
+    const response = await fetch(
+      "/.netlify/functions/pay",
+      {
+        method:"POST",
+
+        headers:{
+          "Content-Type":"application/json"
+        },
+
+        body:JSON.stringify({
+          phone:phone
+        })
+      }
+    );
+
+    const data =
+      await response.json();
+
+    if(data.error){
+
+      status.innerText =
+        "Payment failed";
+
+    }else{
+
+      status.innerText =
+        "Payment request sent!";
+    }
+
+  }catch(err){
+
+    status.innerText =
+      "Connection error";
+  }
+}
